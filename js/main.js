0ae5a9fd4e11db141c197e21280eb9696ba69eb4
lea cohen <cohenswim08@gmail.com>
2022-02-17 22:14:56 -0800

    add popups

--- js/main.js
+++ js/main.js
@@ -10,11 +10,39 @@ require([
     // Data from BGMAPP
     const url = "https://services9.arcgis.com/q5uyFfTZo3LFL04P/arcgis/rest/services/survey123_0954ef4c3eb74d9989a91330c7740a9f/FeatureServer/0";
 
+    // PopupTemplate with some Arcade to check if feature has website field value
+    const template = {
+        title: "{Name}",
+        lastEditInfoEnabled: false,
+        content: [{
+            type: "fields",
+            fieldInfos: [{
+                fieldName: "Address",
+                label: "Address"
+            }, {
+                fieldName: "Industry",
+                label: "Industry"
+            }]
+        }, {
+            type: "text",
+            text: '<b>{expression/has-website}</b> <a href={expression/website-expr}>{expression/website-expr}</a>'
+        }],
+        expressionInfos: [{
+            name: 'website-expr',
+            title: "Website:",
+            expression: 'IIF(!IsEmpty($feature.Website), $feature.Website, null)'
+        }, {
+            name: 'has-website',
+            expression: 'IIf(!IsEmpty($feature.Website), "Website: ", "No website found for this business")'
+        }]
+    };
+
     // Initialize FeatureLayer
     const featureLayer = new FeatureLayer({
         title: "Black-owned Businesses",
         url: url,
-        copyright: "BGMAPP"
+        copyright: "BGMAPP",
+        popupTemplate: template
     })
 
     const map = new Map({
